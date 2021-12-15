8e544374337620240d2982e4b493ccd4bd8cca9b
sunkeeper316 <sunkeeper316@gmail.com>
2021-12-15 21:15:27 +0800

    init

---
+++ player.py
@@ -0,0 +1,31 @@
+from PIL.Image import NONE
+import pyautogui
+import pos
+import time
+
+pos.found_d2rwin
+
+
+time.sleep(1)
+# pyautogui.moveRel(NONE , 375 , duration= 0.5)
+time.sleep(1)
+
+play = pos.found_start()
+
+pyautogui.moveTo(play)
+time.sleep(0.03)
+pyautogui.click()
+
+difficulty = pos.found_pos("nightmare_btn")
+
+pyautogui.moveTo(difficulty )
+time.sleep(0.03)
+pyautogui.click()
+
+pos.found_d2rwin()
+
+                
+def end_game() :
+    pyautogui.keyDown('esc')
+    time.sleep
+    
---
+++ pos.py
@@ -0,0 +1,39 @@
+import pyautogui
+
+wincenter : tuple
+
+def found_start():
+    
+        while 1 :
+            play_pos = pyautogui.locateOnScreen('assets/templates/play_btn.png',grayscale=True, confidence=.8 )
+            play_gary_pos = pyautogui.locateOnScreen('assets/templates/play_btn_gray.png' ,grayscale=True, confidence=.8)
+
+            if  play_pos or  play_gary_pos :
+                print(play_pos)
+
+                print(play_gary_pos)
+                if play_pos :
+                    return  pyautogui.center(play_pos)
+                elif play_gary_pos :
+                    return pyautogui.center(play_gary_pos)
+
+def found_pos(img):
+    
+        while 1 :
+            play_pos = pyautogui.locateOnScreen(f'assets/templates/{img}.png',grayscale=True, confidence=.8 )
+
+            if  play_pos  :
+                return  pyautogui.center(play_pos)
+                    
+def found_d2rwin():
+    
+        while 1 :
+            play_pos = pyautogui.locateOnScreen(f'assets/win/d2wintitle.png',grayscale=True, confidence=.8 )
+
+            if  play_pos  :
+                
+                wincenter = (play_pos.x, play_pos.y + 375)
+                print(wincenter)
+                pyautogui.moveTo(wincenter)
+                break
+                # return  pyautogui.center(play_pos)
