2a2f3c432fdf258805cdfa89edf5c9a6d661197e
huangdesang <sunkeeper316@gmail.com>
2022-01-12 10:29:20 +0800

    20220112

--- test.py
+++ test.py
@@ -2,9 +2,10 @@ import math
 import random
 import keyboard
 import os
+import mss
 # import displaymap
-# import cv2
-# import numpy as np
+import cv2
+import numpy as np
 import pyautogui
 import threading
 # import datetime
@@ -89,168 +90,50 @@ def funnd_grid( row ) :
                 time.sleep(0.3)
                 
     pyautogui.keyUp('ctrl')
-
+def show(imgarray) :
+    cv2.namedWindow("input", cv2.WINDOW_AUTOSIZE)
+    cv2.imshow("input", imgarray)
+    hsv = cv2.cvtColor(imgarray, cv2.COLOR_BGR2HSV)
+    low_hsv = np.array([0,43,46])
+    high_hsv = np.array([10,255,255])
+    mask = cv2.inRange(hsv,lowerb=low_hsv,upperb=high_hsv)
+    cv2.imshow("test",mask)
+    cv2.waitKey(1)
+    cv2.destroyAllWindows()
             
-            
-    # gridcenter = pyautogui.locateCenterOnScreen('assets/templates/grid.png',grayscale=False, confidence=.8) 
-    # grid = pyautogui.locateOnScreen('assets/templates/grid.png',grayscale=False, confidence=.8 , region=( center[0] + 305 , center[1] - 13 , 45 , 45))
+def getColor() :
+    sct = mss.mss()
+    imgarray = np.array(sct.grab((0 , 0 , 300 , 400)))
+    # img = pyautogui.screenshot()
+    # imgarray = np.array(img)
 
-    
-    # gridlist = list(grid)
-    # for g in gridlist :
-    # pyautogui.moveTo(grid , duration = 0.2)
-    # print(len(gridlist))
-    # print(grid)
-    # print(f'grid pos = {grid}')
-    # return grid
-    #(946,347) (306 , -13)
+    # cv2.namedWindow("input", cv2.WINDOW_AUTOSIZE)
+    # cv2.imshow("input", imgarray)
+    hsv = cv2.cvtColor(imgarray, cv2.COLOR_BGR2HSV)
+    low_hsv = np.array([0,43,46])
+    high_hsv = np.array([10,255,255])
+    mask = cv2.inRange(hsv,lowerb=low_hsv,upperb=high_hsv)
+    cv2.imshow("test",mask)
+    # t = threading.Thread(target=show ,args=(imgarray,))
+    # t.start()
+    cv2.imshow("test",mask)
+    cv2.waitKey(1)
+    # print(mask)
+    for pixels in mask :
+        # check = False
+        # shows = []
+        if 255 in pixels :
+            return True
+        
+    return False
 
 if __name__ == "__main__" :
+    print(getColor())
+    for x in range(0,1280) :
+        for y in range(0,720) :
+            print(f"{x},{y}")
+
+    # sct = mss.mss()
+    # sct.grab()
     # for i in range(0 , 2) :
     #     # print(f"x:{i * 42}")
-    #     for j in range(0 , 7) :
-    #         # print(f"y:{j * 42}")
-    #         print(f"(x,y){(i * 42,j * 42)}")
-    # center = pos.found_center()
-    # x = center[0] - 640
-    # print((center[0] , center[1]))
-    # pyautogui.moveTo( (center[0] - 640 , center[1] - 360) )
-    # print(center[0] )
-    _pos = funnd_grid( 4 )
-    # pyautogui.moveTo(_pos)
-
-    # testCallback(3,callback =testRun)
-    # keyboard.add_hotkey('e', lambda: testRun("got key e"))
-    # a = 4
-
-    # print(f'abc{a}')
-    # print(r"ab$$$")
-    # list = [1,4,6]
-    # print(methodT1("d"))
-    # print(methodT1("d",list,c = "g" , h = "r"))
-
-    # while 1 :
-    #     a = 1
-    # zero = (0,0)
-    # a_pos = (640,360)
-    # b_pos = (54,80)
-    # _hypot =eee math.hypot(30,40)
-    # d = math.dist(a_pos , b_pos)
-    
-    # print(f"a_pos 距離 {math.dist(a_pos,zero)}")
-    # print(math.hypot(a_pos[0] ,a_pos[1] ))
-    # print(math.hypot(b_pos[0] ,b_pos[1] ))
-    # getPosition()
-    # getMouse()
-
-    # center = found_zero()
-    # while 1 :
-    #     square_pos = pyautogui.locateCenterOnScreen('tests/Square.png')
-    #     if square_pos :
-    #         print(f"square_pos {square_pos}")
-    #         break
-
-    
-    
-
-
-
-# def jop() :
-#     now = datetime.datetime.now()
-#     ts = now.strftime('%Y/%m/%d %H:%M:%S')
-#     print(f"now {ts}")
-
-# def loopTest() :
-#     s = sched.scheduler(time.time , time.sleep)
-#     s.enter(2,1,jop , ())
-#     s.run()
-    
-# def timerGO() :
-#     timer = threading.Timer(2 , jop)
-#     timer.start()
-# if __name__ == "__main__" :
-#     count = 0
-#     while count < 5 :
-#         count += 1
-#         loopTest()
-    # timerGO()
-    
-
-
-# # displaymap.read_directory("assets/wp/")
-
-# # 開啟網路攝影機
-# cap = cv2.VideoCapture(0)
-
-# # 設定影像尺寸
-# width = 1280
-# height = 960
-
-# # 設定擷取影像的尺寸大小
-# cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
-# cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
-
-# # 計算畫面面積
-# area = width * height
-
-# # 初始化平均影像
-# ret, frame = cap.read()
-# avg = cv2.blur(frame, (4, 4))
-# avg_float = np.float32(avg)
-
-# while(cap.isOpened()):
-#   # 讀取一幅影格
-#   ret, frame = cap.read()
-
-#   # 若讀取至影片結尾，則跳出
-#   if ret == False:
-#     break
-
-#   # 模糊處理
-#   blur = cv2.blur(frame, (4, 4))
-
-#   # 計算目前影格與平均影像的差異值
-#   diff = cv2.absdiff(avg, blur)
-
-#   # 將圖片轉為灰階
-#   gray = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)
-
-#   # 篩選出變動程度大於門檻值的區域
-#   ret, thresh = cv2.threshold(gray, 25, 255, cv2.THRESH_BINARY)
-
-#   # 使用型態轉換函數去除雜訊
-#   kernel = np.ones((5, 5), np.uint8)
-#   thresh = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel, iterations=2)
-#   thresh = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel, iterations=2)
-
-#   # 產生等高線
-#   cntImg, cnts, _ = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-#   for c in cnts:
-#     # 忽略太小的區域
-#     if cv2.contourArea(c) < 2500:
-#       continue
-
-#     # 偵測到物體，可以自己加上處理的程式碼在這裡...
-
-#     # 計算等高線的外框範圍
-#     (x, y, w, h) = cv2.boundingRect(c)
-
-#     # 畫出外框
-#     cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-
-#   # 畫出等高線（除錯用）
-#   cv2.drawContours(frame, cnts, -1, (0, 255, 255), 2)
-
-#   # 顯示偵測結果影像
-#   cv2.imshow('frame', frame)
-
-#   if cv2.waitKey(1) & 0xFF == ord('q'):
-#     break
-
-#   # 更新平均影像
-#   cv2.accumulateWeighted(blur, avg_float, 0.01)
-#   avg = cv2.convertScaleAbs(avg_float)
-
-# cap.release()
-# cv2.destroyAllWindows()v
