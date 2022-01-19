8ed6a4f4721f903925906d40fdaac40e8fa48302
huangdesang <sunkeeper316@gmail.com>
2022-01-19 18:43:48 +0800

    文字辨識測試

--- test.py
+++ test.py
@@ -4,18 +4,12 @@ from cv2 import TermCriteria_COUNT
 import cv2
 import keyboard
 import os
-<<<<<<< HEAD
 import mss
+from PIL import Image
 # import displaymap
 import cv2
 import numpy as np
-=======
-from PIL import Image
-# import displaymap
-# import cv2
-import numpy as np
-from numpy.core.multiarray import array
->>>>>>> ec894e6312860a1be9576d3b7f5e8cedde89b48f
+import pytesseract
 import pyautogui
 import threading
 # import datetime
@@ -137,18 +131,9 @@ def getColor() :
         
     return False
 
-<<<<<<< HEAD
-if __name__ == "__main__" :
-    print(getColor())
-    for x in range(0,1280) :
-        for y in range(0,720) :
-            print(f"{x},{y}")
 
-    # sct = mss.mss()
-    # sct.grab()
-    # for i in range(0 , 2) :
-    #     # print(f"x:{i * 42}")
-=======
+
+
 def found_img(img , confidence ,timeout = 20):
         start = time.time()
         while (time.time() - start) < timeout:
@@ -210,101 +195,22 @@ def found_color(img , low , high) :
     return False
 
 
-
 if __name__ == "__main__" :
-    center = 'assets/win/d2rwinlogo.png'
-    center_pos = found_img(center , .8)
-    # img = cv2.imread('assets/items/rune_2_eld.png')
-    # img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    # im_pil = Image.fromarray(img)
-
-    # print(im_pil.size )
-    
-    # for x in range(0 , im_pil.size[0]) :
-    #     for y in range(0 , im_pil.size[1]) :
-    #         print(im_pil.getpixel((x,y)))
-    pos.found_center()
-    time.sleep(1)
-    orangelow = [20,190,190]
-    orangehigh = [23,255,255]
-    result = found_color(pyautogui.screenshot(region=(center_pos.left , center_pos.top + center_pos.height , 1280 , 720)) , orangelow , orangehigh)
-
-    print(f"result = {result}")
-
-    # pyautogui.screenshot()
-    # print(pyautogui.screenshot())
-    # topleft = 'assets/templates/main_menu_top_left.png'
-    # topright = 'assets/templates/main_menu_top_right.png'
-    # bottomleft = 'assets/templates/main_menu_bottom_left.png'
-
-    # itembag = 'assets/templates/itembag.png'
-    # bagtopright = 'assets/templates/bagtopright.png'
-
-    # itembag_pos = found_img(bagtopright , .85)
-    # time.sleep(0.1)
-    # print(f'itembag_pos座標={itembag_pos}')
-    # time.sleep(0.1)
-
-    # right = itembag_pos.left + itembag_pos.width
-
-    # benchmark = (right - 355 , itembag_pos.top + 380)
-
-    # put_store(benchmark, 2 )
-
-    # im = pyautogui.screenshot()
-    
-    # print(f"img = {im.getpixel()}")
-    # pyautogui.moveTo((right - 355  , itembag_pos.top + 380) , duration= 0.2)
-
-    # time.sleep(0.1)
-
-    # pyautogui.keyDown('ctrl')
-    # time.sleep(0.1)
-    # pyautogui.click()
-    # time.sleep(0.1)
-    # pyautogui.keyUp('ctrl')
-    # time.sleep(0.1)
-
-
-    # center = 'assets/win/d2wintitle.png'
-    # center_pos = found_img(center , .7)
-    # time.sleep(0.1)
-    # print(f'座標={center_pos}')
-    # pyautogui.moveTo(center_pos , duration=0.2)
-   
-    # topleft_pos = found_img(topleft , .7)
-    # time.sleep(0.1)
-    # print(f'topleft_pos座標={topleft_pos}')
-    # pyautogui.moveTo(topleft_pos , duration=0.2)
-    # time.sleep(0.1)
-
-    # topright_pos = found_img(topright , .7)
-    # time.sleep(0.1)
-    # print(f'topright_pos座標={topright_pos}')
-    # pyautogui.moveTo(topright_pos , duration=0.2)
-    # time.sleep(0.1)
-
-    # bottomleft_pos = found_img(bottomleft , .7)
-    # time.sleep(0.1)
-    # print(f'bottomleft_pos座標={bottomleft_pos}')
-    # pyautogui.moveTo(bottomleft_pos , duration=0.2)
-    # time.sleep(0.1)
-
-    # height = bottomleft_pos.top - topleft_pos.top + bottomleft_pos.height
-    # wedth = topright_pos.left - topleft_pos.left + topright_pos.width
-
-    # print(f'height={height}')
-    # print(f'wedth={wedth}')
-
-
-
-    # center = pos.found_center()
-    # pyautogui.moveTo(center)
-    # time.sleep(0.3)
-    # pyautogui.moveRel(250 , 0 , duration=0.2)
-    # time.sleep(0.3)
-    # pyautogui.press('e')
+    # img = Image.open('assets/items/bl__magic_boneweave.png')
+    # text = pytesseract.image_to_string(img, lang='eng')
 
     
-    
->>>>>>> ec894e6312860a1be9576d3b7f5e8cedde89b48f
+    # print(text)
+
+    for i in range(1 , 10000) :
+        if i % 2 == 0 :
+            print(i)
+    # print(getColor())
+    # for x in range(0,1280) :
+    #     for y in range(0,720) :
+    #         print(f"{x},{y}")
+
+    # sct = mss.mss()
+    # sct.grab()
+    # for i in range(0 , 2) :
+    #     # print(f"x:{i * 42}")
